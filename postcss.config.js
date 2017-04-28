266fff8fd0b4c483169df91f227e80dda8900c17
Wes Dearborn <hwdearborn@gmail.com>
2017-04-28 12:33:28 -0500

    add class prefix, gitignore

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports =  {
+  "map": false,
+  "plugins": {
+    "postcss-simple-vars": {},
+    "postcss-extend": {},
+    "postcss-import": {},
+    "postcss-mixins": {},
+    "postcss-nested": {},
+    "postcss-hexrgba": {},
+    "postcss-inline-svg": {},
+    "postcss-strip-inline-comments": {},
+    "postcss-flexbugs-fixes": {},
+    "postcss-prefixer": 'rm-',
+    "cssnano": {}
+  }
+}
